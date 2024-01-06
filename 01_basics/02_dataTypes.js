cdea0ff150edec5b18f23e47ab3a82b865b685e1
Krunal Patel <63594093+KrunalKaPatel@users.noreply.github.com>
2024-01-06 07:34:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // this will treat all JS code as newer version or newer JS
+
+// alert( 3 + 3);  //We are using nodeJS, not browser
+
+console.log( 3 + 3);  // code readiablity should be high
+console.log("Krunal");
+
+
+let name = "Krunal" // srting datatype
+let age = 18 // int datatype
+let isLoggedIn = false // boolean datatype
+
+
+// number => 2 to power 53
+// bigint
+// string => use in "" this
+// boolean => true/false
+// null => standalone value 
+// undefined => no value assign yet
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   //object
